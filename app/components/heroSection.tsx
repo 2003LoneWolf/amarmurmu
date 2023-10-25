f085eb59bfcb826705243b1b78ca057845254e19
Amar Murmu <iron57441@gmail.com>
2023-10-25 21:12:39 +0530

    #

--- app/components/heroSection.tsx
+++ app/components/heroSection.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 function HeroSection() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
-        <div className='text-center font-sans text-[96px]'>
-        Imagination, Design, and develop –<div className='text-[#8338EC]'>  That's My Superpower! </div>       </div>
+        <div className=' text-center font-sans text-[96px]'>
+        Imagination, Design, and develop – <span className='text-[#8338EC]'> That&apos;s My Superpower! </span>
+         </div>
         <div>
 
         </div>
