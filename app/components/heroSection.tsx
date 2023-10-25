ab7e6d5e08fef992eb19c8dadeca65624d8b104e
Amar Murmu <iron57441@gmail.com>
2023-10-25 21:00:22 +0530

    #

--- app/components/heroSection.tsx
+++ app/components/heroSection.tsx
@@ -4,7 +4,7 @@ function HeroSection() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
         <div className='text-center font-sans text-[96px]'>
-        Imagination, Design, and develop –<span className='text-[#8338EC]'>  That's My Superpower! </span>       </div>
+        Imagination, Design, and develop –<div className='text-[#8338EC]'>  That's My Superpower! </div>       </div>
         <div>
 
         </div>
